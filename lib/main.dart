64e168c2ad84c8283162b8f3b46d507a8fca999c
SKurbatova <58175476+SofiFairyTell@users.noreply.github.com>
2023-06-05 16:21:19 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:recrutterapp/screens/CandidatScreen.dart';
 import 'package:recrutterapp/screens/ProfileScreen.dart';
 import 'package:recrutterapp/screens/SignUpScreen.dart';
