41bdfef5736a7269ce26c9e1757181412fbbbbf0
SKurbatova <58175476+SofiFairyTell@users.noreply.github.com>
2023-06-07 10:28:11 +0300

    Create PositionDetails.dart

---
+++ lib/screens/PositionDetails.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'DrawerWidget.dart';
+
+import 'Widgets/AppBarCustom.dart';
+import 'Widgets/CardWidget.dart';
+
+final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+class PositionDetails extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
